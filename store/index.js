1685d1aa7a0dbaea640571af242afa63b1bd68c4
mashsh <osamu.kondo@aplio.co.jp>
2020-03-20 15:15:20 +0900

    flat部分修正

--- store/index.js
+++ store/index.js
@@ -275,10 +275,25 @@ export const actions = {
     }
     // 配列で返ってくる為、ネストを削除する
     console.log(" articlesInserarhReasurlrl ~================", articlesInSearchResults);
-    articlesInSearchResults = articlesInSearchResults.flat();
+    // articlesInSearchResults = articlesInSearchResults.flat();
 
-    commit('setArticlesInSearchResults', articlesInSearchResults);
-    commit('setNumberOfHitArticles', articlesInSearchResults.length);
+    // var arr = [[{ name: "php" }], [{ name: "java" }]];
+
+    // var flatArr = [];
+
+    let flatResults = [];
+    for (let i = 0; i < articlesInSearchResults.length; i++) {
+      console.log('iについて~~~~~~~~~~', articlesInSearchResults[i][0]);
+      
+      flatResults.push(articlesInSearchResults[i][0]);
+    }
+    console.log('flatResults ~~~~~~~~~~~~', flatResults);
+    
+
+    // commit('setArticlesInSearchResults', articlesInSearchResults);
+    // commit('setNumberOfHitArticles', articlesInSearchResults.length);
+    commit('setArticlesInSearchResults', flatResults);
+    commit('setNumberOfHitArticles', flatResults.length);
   },
 
   countNumberOfHitArticles({ commit, state }) {
